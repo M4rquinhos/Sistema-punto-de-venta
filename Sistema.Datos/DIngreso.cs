f76d6bfcf07e15aed9375cb10677eec1e1f6f304
Marco Carrillo <marco.37099p@gmail.com>
2023-01-05 19:42:37 -0600

    Acceso a datos ingreso

---
+++ Sistema.Datos/DIngreso.cs
@@ -0,0 +1,128 @@
+﻿using Sistema.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Datos
+{
+    public class DIngreso
+    {
+        public DataTable Listar()
+        {
+            SqlDataReader resultado;
+            DataTable tabla = new DataTable();
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon = Conexion.GetInstancia().CrearConexion(); //Creando la conexion con la BD
+                SqlCommand comando = new SqlCommand("ingreso_listar", sqlCon); //recibe que es lo que se quiere hacer (SP O QUERY)
+                comando.CommandType = CommandType.StoredProcedure; //Se indica que es un SP
+                sqlCon.Open();
+                resultado = comando.ExecuteReader();
+                tabla.Load(resultado);
+                return tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open) sqlCon.Close();
+            }
+        }
+
+        public DataTable Buscar(string valor)
+        {
+            SqlDataReader resultado;
+            DataTable tabla = new DataTable();
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon = Conexion.GetInstancia().CrearConexion();
+                SqlCommand comando = new SqlCommand("ingreso_buscar", sqlCon);
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.Add("@valor", SqlDbType.VarChar).Value = valor;
+                sqlCon.Open();
+                resultado = comando.ExecuteReader();
+                tabla.Load(resultado);
+                return tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open) sqlCon.Close();
+            }
+        }
+
+        public string Insertar(Ingreso Obj)
+        {
+            string respuesta = "";
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon = Conexion.GetInstancia().CrearConexion();
+                SqlCommand comando = new SqlCommand("ingreso_insertar", sqlCon);
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.Add("@idproveedor", SqlDbType.Int).Value = Obj.IdProveedor;
+                comando.Parameters.Add("@idusuario", SqlDbType.Int).Value = Obj.IdUsuario;
+                comando.Parameters.Add("@tipo_comprobante", SqlDbType.VarChar).Value = Obj.TipoComprobante;
+                comando.Parameters.Add("@serie_comprobante", SqlDbType.VarChar).Value = Obj.SerieComprobante;
+                comando.Parameters.Add("@num_comprobante", SqlDbType.VarChar).Value = Obj.NumComprobante;
+                comando.Parameters.Add("@ipuesto", SqlDbType.VarChar).Value = Obj.Impuesto;
+                comando.Parameters.Add("@total", SqlDbType.VarChar).Value = Obj.Total;
+                comando.Parameters.Add("@detalle", SqlDbType.Structured).Value = Obj.Detalles;
+                sqlCon.Open();
+                respuesta = comando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo insertar el registro";
+                return respuesta;
+
+            }
+            catch (Exception ex)
+            {
+
+                respuesta = ex.Message;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open) sqlCon.Close();
+            }
+            return respuesta;
+        }
+
+        public string Anular(int id)
+        {
+            string respuesta = "";
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon = Conexion.GetInstancia().CrearConexion();
+                SqlCommand comando = new SqlCommand("ingreso_anular", sqlCon);
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.Add("@idingreso", SqlDbType.Int).Value = id;
+                sqlCon.Open();
+                respuesta = comando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo anular el registro";
+                return respuesta;
+
+            }
+            catch (Exception ex)
+            {
+
+                respuesta = ex.Message;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open) sqlCon.Close();
+            }
+            return respuesta;
+        }
+    }
+}
